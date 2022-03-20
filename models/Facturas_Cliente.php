33850c2538f8187d143f301bc5abeabee3a30a55
AnnerIscoa <94268000+AnnerIscoa@users.noreply.github.com>
2022-03-19 22:41:44 -0600

    Creacion de ma_facturas_cliente

---
+++ models/Facturas_Cliente.php
@@ -0,0 +1,69 @@
+<?php 
+
+
+class Facturas extends Conectar{
+
+    public function get_facturas(){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM ma_facturas_cliente ";
+        $sql=$conectar->prepare($sql);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function get_factura($id){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM ma_facturas_cliente WHERE ID = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $id);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insert_factura($numero_factura, $id_socio,$fecha_factura, $detalle, $sub_total, $total_isv, $total, $fecha_vencimiento, $estado){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="INSERT INTO ma_facturas_cliente(id, numero_factura,id_socio, fecha_factura,detalle, sub_total,total_isv, total, fecha_vencimiento, estado)
+        VALUES (NULL,?,?,?,?,?,?,?,?,?);";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $numero_factura);
+        $sql->bindValue(2, $id_socio);
+        $sql->bindValue(3, $fecha_factura);
+        $sql->bindValue(4, $detalle);
+        $sql->bindValue(5, $sub_total);
+        $sql->bindValue(6, $total_isv);
+        $sql->bindValue(7, $total);
+        $sql->bindValue(8, $fecha_vencimiento);
+        $sql->bindValue(9, $estado);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function update_factura($id, $detalle, $total, $estado){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="UPDATE ma_facturas_cliente SET detalle=?,total=?,estado =? WHERE ID= ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $detalle);
+        $sql->bindValue(2, $total);
+        $sql->bindValue(3, $estado);
+        $sql->bindValue(4, $id);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function delete_factura($id){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="DELETE FROM ma_facturas_cliente WHERE ID = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $id);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+
+?>
