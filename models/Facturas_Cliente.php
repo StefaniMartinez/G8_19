f11b7aae2845732148e233337058901611b51fb3
AnnerIscoa <94268000+AnnerIscoa@users.noreply.github.com>
2022-03-26 10:36:22 -0600

    Ultima actualizacion de Facturas_Clientes

--- models/Facturas_Cliente.php
+++ models/Facturas_Cliente.php
@@ -41,15 +41,21 @@ class Facturas extends Conectar{
         return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function update_factura($id, $detalle, $total, $estado){
+    public function update_factura($id, $numero_factura, $id_socio,$fecha_factura, $detalle, $sub_total, $total_isv, $total, $fecha_vencimiento, $estado){
         $conectar= parent::conexion();
         parent::set_names();
-        $sql="UPDATE ma_facturas_cliente SET detalle=?,total=?,estado =? WHERE ID= ?";
+        $sql="UPDATE ma_facturas_cliente SET numero_factura=?,id_socio=?,fecha_factura=?,detalle=?,sub_total=?,total_isv=?,total=?,fecha_vencimiento=?,estado =? WHERE ID= ?";
         $sql=$conectar->prepare($sql);
-        $sql->bindValue(1, $detalle);
-        $sql->bindValue(2, $total);
-        $sql->bindValue(3, $estado);
-        $sql->bindValue(4, $id);
+        $sql->bindValue(1, $numero_factura);
+        $sql->bindValue(2, $id_socio);
+        $sql->bindValue(3, $fecha_factura);
+        $sql->bindValue(4, $detalle);
+        $sql->bindValue(5, $sub_total);
+        $sql->bindValue(6, $total_isv);
+        $sql->bindValue(7, $total);
+        $sql->bindValue(8, $fecha_vencimiento);
+        $sql->bindValue(9, $estado);
+        $sql->bindValue(10, $id);
         $sql->execute();
         return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
     }
